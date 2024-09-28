af06eb52b1489a8e92af6c552eb360d20914553e
vok889 <vok889@gmail.com>
2024-09-28 17:06:14 +0700

    define validation

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,19 @@
-export class CreateItemDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateItemDto {
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    amount: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number;
+
+    @IsOptional()
+    contactMobileNo: string;
+}
