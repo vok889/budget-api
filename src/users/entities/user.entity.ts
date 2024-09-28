b02671545a56c0bc0651078fe8a197740d05dbda
vok889 <vok889@gmail.com>
2024-09-28 17:32:59 +0700

    add create users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum Role {
+    USER = 'USER',
+    ADMIN = 'ADMIN'
+}
+
+@Entity('bg_user')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({
+        unique: true
+    })
+    username: string;
+    
+    @Column()
+    password: string;
+    
+    @Column()
+    role: Role
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+    
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }    
+    
+}
