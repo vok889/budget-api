13e9738c32d0b068957791da7f6897ef106bd4ae
vok889 <vok889@gmail.com>
2024-09-28 11:23:56 +0700

    gen db module

---
+++ src/db/db.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+            type: 'postgres',
+            host: config.get('DB_HOST'),
+            port: config.get('DB_PORT'),
+            username: config.get('DB_USERNAME'),
+            password: config.get('DB_PASSWORD'),
+            database: config.get('DB_DATABASE'),
+            autoLoadEntities: config.get('DB_AUTO_LOAD_ENTITIES'),
+            synchronize: config.get('DB_SYNCHRONIZE')
+        })
+    })]
+})
+export class DbModule {}
